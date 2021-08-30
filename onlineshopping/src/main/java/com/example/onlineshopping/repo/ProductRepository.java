57470e46d55dc35b7452f33fdcc7bd78867fd0fe
SHAIK MOHMMAD ARIF <70676142+Sarif747@users.noreply.github.com>
2021-08-30 17:38:48 +0530

    New Commit

--- onlineshopping/src/main/java/com/example/onlineshopping/repo/ProductRepository.java
+++ onlineshopping/src/main/java/com/example/onlineshopping/repo/ProductRepository.java
@@ -1,10 +1,10 @@
 package com.example.onlineshopping.repo;
+
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.example.onlineshopping.model.Product;
 
-
 public interface ProductRepository extends JpaRepository<Product, Integer> {
 
-	
 }
